07a6d07f6e83860daae042415cef6bb7e3c5f9ca
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-08-25 18:52:58 -0400

    create Swatch component

---
+++ components/Swatch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="swatch" :style="`background-color: ${blue.value}`">{{
+    blue.title
+  }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Swatch',
+  props: {
+    blue: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
