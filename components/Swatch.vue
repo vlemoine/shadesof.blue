e434fc020ced409c41369e194bd65382beea6eb8
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-08-29 00:18:46 -0400

    Update Swatch.vue
    change text color based on luminosity

--- components/Swatch.vue
+++ components/Swatch.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="swatch" :style="`background-color: ${blue.value}`">{{
+  <div class="swatch p-2" :style="`background-color: ${blue.value};color: ${color}`">{{
     blue.title
-  }}</div>
+  }}
+  <slot />
+  </div>
 </template>
 
 <script>
+import Color from 'color';
+
 export default {
   name: 'Swatch',
   props: {
     blue: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    color() {
+      const c = Color(this.blue.value)
+      return c.isLight() ? '#111' : '#fff'
     }
   }
 }
