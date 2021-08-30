0d712da2f2760cbd3dbd86e51a0d4d993140c84e
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-08-30 14:15:15 -0400

    Add filters

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,57 @@
 <template>
   <section class="swatches grid p-8">
-    <template v-for="(blue, i) in c">
-      <div :key="blue.hex" class="relative" >
+    <header class="swatches__header flex">
+      <p>{{ c.length }} blues documented!</p>
+      <div id="filters" class="filters ml-auto">
+        <label
+          >Show grays
+          <input
+            v-model="filters.includeGrays"
+            :disabled="filters.onlyGrays"
+            type="checkbox"
+          />
+        </label>
+        <label
+          >Only grays
+          <input v-model="filters.onlyGrays" type="checkbox" />
+        </label>
+        <label
+          >Show out of bounds
+          <input v-model="filters.includeOobs" type="checkbox" />
+        </label>
+      </div>
+    </header>
+
+    <template v-for="blue in c">
+      <div
+        :key="blue.hex"
+        class="relative"
+        :class="{
+          hidden:
+            (blue.gray && !filters.includeGrays && !filters.onlyGrays) ||
+            (!blue.gray && filters.onlyGrays)
+        }"
+      >
         <Swatch
           :blue="blue"
-          :style="
-            `
-          ${
-            blue.hsl.h > 240 || blue.hsl.h < 180
-              ? 'box-shadow:inset 0 0 0 5px red;'
-              : ''
-          }
-          ${
-            (blue.hsl.s <= 25.2 || blue.hsl.l <= 15)
-              ? 'box-shadow: inset 0 0 0 5px orange;'
-              : ''
-          }
-          ${
-            false && blue.hsl.s > 20 && blue.hsl.s < 31
-              ? 'box-shadow: inset 0 0 0 5px purple;'
-              : ''
-          }
-        `
-          "
+          class="relative"
         >
-          <span v-if="false">{{ i }}</span>
+          <div class="markers absolute top-1 right-1 opacity-50 flex flex-col">
+            <i v-if="blue.gray" class="fas fa-adjust mb-1"></i>
+            <i v-if="blue.oob" class="fas fa-rainbow"></i>
+          </div>
+
           <div v-if="labels" class="labels">
-            <strong>{{ blue.title }}</strong><br>
+            <h2 class="pr-3 font-bold">{{ blue.title }}</h2>
             <span>{{ blue.hex }}</span
             ><br />
-            <span>{{ Math.round(blue.hsl.h) }}</span>
-            <span>{{ blue.hsl.s }}</span>
+            <span>h {{ Math.round(blue.hsl.h) }}</span>
+            <!-- <span>{{ blue.hsl.s }}</span> -->
             <template v-if="details">
               <br />
-              <span>{{ Math.round(blue.hsl.s) }}</span
+              <span>s {{ Math.round(blue.hsl.s) }}</span
               ><br />
-              <span>{{ Math.round(blue.hsl.l) }}</span>
+              <span>l {{ Math.round(blue.hsl.l) }}</span>
             </template>
           </div>
         </Swatch>
@@ -63,24 +79,33 @@ export default {
   data() {
     return {
       labels: true,
-      details: true
+      details: true,
+      filters: {
+        includeGrays: true,
+        onlyGrays: false,
+        includeOobs: true
+      }
     };
   },
   computed: {
     c() {
-      // let x = this.x11.slice(0);
-      // let p = this.pantone.slice();
       let sort = this.x11.concat(this.pantone);
-      sort = sort.concat(this.tcx)
+      sort = sort.concat(this.tcx);
       sort.forEach(s => {
         const b = Color(s.value);
         const hsl = b.hsl().object();
         s.hex = b.hex();
         s.luminosity = b.luminosity();
-        s.hsl = hsl;
+        s.hsl = {};
+        s.hsl.h = Math.round(hsl.h);
+        s.hsl.s = Math.round(hsl.s);
+        s.hsl.l = Math.round(hsl.l);
+        s.gray =
+          s.hsl.s <= 22 ||
+          s.hsl.l <= 10 ||
+          (s.hsl.l > 10 && s.hsl.l <= 15 && s.hsl.s <= 50);
+        s.oob = s.hsl.h < 180 || s.hsl.h > 240;
       });
-      // sort = sort.sort((a, b) => a.hsl.h - b.hsl.h);
-      // sort = sort.sort((a, b) => a.hsl.s - b.hsl.s);
       sort = sort.sort((a, b) => b.hsl.l - a.hsl.l);
       sort = sort.sort((a, b) => a.hsl.s - b.hsl.s);
       sort = sort.sort((a, b) => a.hsl.h - b.hsl.h);
@@ -91,6 +116,9 @@ export default {
 </script>
 
 <style scoped>
+.swatches__header {
+  grid-column: 1/-1;
+}
 .swatches {
   grid-template-columns: repeat(auto-fill, minmax(5rem, 1fr));
 }
@@ -104,13 +132,13 @@ export default {
 .swatch:hover,
 .swatch:focus {
   position: absolute;
-  width: 8rem;
-  height: 8rem;
+  width: 9rem;
+  height: 9rem;
   overflow: initial;
   z-index: 2;
-  left: -1rem;
-  top: -1rem;
-  box-shadow: 0 0 2rem -.1rem #0008;
+  left: -1.5rem;
+  top: -1.5rem;
+  box-shadow: 0 0 2rem -0.1rem #0008;
 }
 .swatch:hover .labels,
 .swatch:focus .labels {
