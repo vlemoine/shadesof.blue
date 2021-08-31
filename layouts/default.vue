9b74e4a3a55d8477c1bb48aab2e1424e33ae3cdd
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-08-30 23:13:13 -0400

    Update default.vue
    Remove header, to be in own component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,17 @@
 <template>
   <main class="main">
-    <header class="flex p-4">
-      <Logo />
-      <h1 class="ml-4 text-2xl font-bold">Shades of Blue</h1>
-    </header>
-    
-    <Nuxt/>
+    <Nuxt />
   </main>
 </template>
 
-<script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: { Logo },
-}
-</script>
-
 <style lang="scss">
-  html {
-    background-color: #eee;
-    color: #111;
-    font-family: 'Anonymous Pro', monospace;
-    &.dark {
-      background-color: #111;
-      color: #fff;
-    }
+html {
+  background-color: #eee;
+  color: #111;
+  font-family: "Anonymous Pro", monospace;
+  &.dark {
+    background-color: #111;
+    color: #fff;
   }
-  header {
-    align-items: center;
-  }
+}
 </style>
