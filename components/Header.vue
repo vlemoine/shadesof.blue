fbedbc661bc9875209e55d10e24bdb3135f41cef
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-09-04 23:46:01 -0400

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -5,8 +5,8 @@
         <h1 class="ml-4 text-2xl font-bold">Shades of Blue</h1></NuxtLink
       >
       <nav class="flex ml-auto items-center gap-4">
-        <NuxtLink to="random" class="font-bold text-lg">Random</NuxtLink>
-        <NuxtLink to="about" class="font-bold text-lg">About</NuxtLink>
+        <NuxtLink to="/random" class="font-bold text-lg">Random</NuxtLink>
+        <NuxtLink to="/about" class="font-bold text-lg">About</NuxtLink>
       </nav>
     </header>
 </template>
