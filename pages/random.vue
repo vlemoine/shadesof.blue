9a4ffd0e806f6df484071e8ce00b845b423481ee
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-09-04 23:51:05 -0400

    Update random.vue

--- pages/random.vue
+++ pages/random.vue
@@ -21,7 +21,7 @@ export default {
       randomArr(pantone).slug,
       randomArr(crayola).slug
     ];
-    window.location = randomArr(random);
+    window.location = `/${randomArr(random)}`;
   }
 };
 </script>
