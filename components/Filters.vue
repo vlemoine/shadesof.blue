b07ea7392571e0a15893cbbdcada68a8a42ecf8f
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-09-08 19:40:56 -0400

    Create Filters.vue

---
+++ components/Filters.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="Filters">
+    <button class="appearance-none" @click="open = true">
+        Filters
+        <i class="fas fa-filter ml-1"></i>
+      </button>
+<div
+      class="
+        filters
+        fixed
+        top-0
+        right-0
+        h-screen
+        z-10
+        bg-black
+        shadow-xl
+        bg-opacity-90
+        transition-all
+      "
+      :class="{
+        'overflow-hidden w-0': !open,
+        'p-4 w-1/3': open,
+      }"
+    >
+      <div class="flex items-center mb-4">
+        <h2 class="text-xl font-bold">Filters</h2>
+        <button
+          class="apperance-none ml-auto text-2xl"
+          @click="open = false"
+        >
+          <i class="fal fa-times"></i>
+        </button>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    }
+  }
+}
+</script>
