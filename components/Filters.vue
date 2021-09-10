db228c31a74c8f726edc858e122d8b752c704828
Vivien <1494277+vlemoine@users.noreply.github.com>
2021-09-10 15:23:19 -0400

    Update Filters.vue

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,44 +1,23 @@
 <template>
-  <div id="Filters">
-    <button class="appearance-none" @click="open = !open">
-      Options
-      <i class="fas fa-filter"></i>
-    </button>
-    <div
-      class="
-        fixed
-        top-0
-        right-0
-        shadow-xl
-        h-screen
-        z-10
-        bg-opacity-90
-        transition-all
-        bg-white
-        dark:bg-black
-      "
-      :class="{
-        'w-0 overflow-hidden': !open,
-        'w-1/3 p-4': open,
-      }"
-    >
-      <div class="flex items-center mb-4">
-        <h2 class="text-xl font-bold">Options</h2>
-        <button class="apperance-none ml-auto text-2xl" @click="open = false">
-          <i class="fal fa-times"></i>
-        </button>
-      </div>
-      <slot></slot>
-    </div>
+  <div
+    id="Filters"
+    class="w-full bg-opacity-90 transition-all bg-white dark:bg-black"
+    :class="{
+      'h-0 overflow-hidden': !open,
+      'p-4': open,
+    }"
+  >
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      open: false,
-    };
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+    },
   },
 };
 </script>
