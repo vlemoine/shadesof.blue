57f6df899a4e38d723f16a3fc0d2dd3e411e1cce
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-05-20 19:42:48 -0400

    Create Fill.vue
    fill template

---
+++ components/Fill.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="text-center text-2xl w-full h-full p-12"
+    :class="text"
+    :style="`background-color:${color.value}`"
+  >
+    <p>{{ hex }}</p>
+    <p v-if="color.alias">AKA {{ color.alias }}</p>
+    <p>From {{ color.source }}</p>
+  </div>
+</template>
+
+<script>
+import Color from "color";
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    text() {
+      const c = Color(this.color.value);
+      return `text-${c.isLight() ? "black" : "white"}`;
+    },
+    hex() {
+      const b = Color(this.color.value);
+      return b.hex();
+    }
+  }
+};
+</script>
