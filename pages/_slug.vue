c57bb712aa2bfee1b5098676ad67956af5df9809
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-05-20 19:43:06 -0400

    Update _slug.vue

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,16 +1,35 @@
 <template>
-  <section class="px-9">
+  <section
+    :class="disam ? '' : 'p-12'"
+    :style="disam ? '' : `background-color:${color.value}`"
+  >
+    <h1 class="hidden">{{ query }}</h1>
     <template v-if="color">
-      <template v-if="!Array.isArray(color)">
-        <pre>{{ color.slug }}</pre>
+      <template v-if="!disam">
+        <Fill :color="color"/>
+        <div class="text-center text-2xl" :class="text">
+          <span class="text-5xl font-bold">
+            {{ color.title }}
+          </span>
+          <p>{{ toHex(color.value) }}</p>
+          <p v-if="color.alias">AKA {{ color.alias }}</p>
+          <p>From {{ color.source }}</p>
+          
+        </div>
       </template>
-      <template v-else>
-        <p>
-          There are {{ color.length }} colors with the name <strong>{{query
-          }}</strong>.
-        </p>
-      </template>
-      <pre>{{ color }}</pre>
+      <div v-else class="h-full flex flex-col">
+        <div class="h-12 flex items-center justify-center">
+          <p>
+            There are {{ color.length }} colors with the name
+            <strong>{{ query }}</strong
+            >.
+          </p>
+        </div>
+        <div class="flex h-full">
+          <Fill v-for="(color, i) in color"
+            :key="i" :color="color" />
+        </div>
+      </div>
     </template>
     <p v-else-if="!about">That's not a shade of blue!</p>
     <nuxt-content :document="about" />
@@ -18,6 +37,7 @@
 </template>
 
 <script>
+import Color from "color";
 export default {
   async asyncData({ $content, params }) {
     let about;
@@ -55,20 +75,42 @@ export default {
         ...crayola,
         ...sw,
       ];
-      lib = lib.filter((e) => (e.slug === params.slug || e.alias === params.slug));
+      lib = lib.filter(
+        (e) => e.slug === params.slug || e.alias === params.slug
+      );
       color = lib.length === 1 ? lib[0] : lib;
-      query = color?.title?.search('/') === -1 ? color.title : lib[lib.length - 1].title
+      query =
+        color?.title?.search("/") === -1
+          ? color.title
+          : lib[lib.length - 1].title;
     }
     return {
       about,
       color,
-      query
+      query,
     };
   },
   head() {
-      return {
-        title: this.about ? 'About shadesof.blue' : `${this.query} - shadesof.blue`,
-      }
+    return {
+      title: this.about
+        ? "About shadesof.blue"
+        : `${this.query} 🔹 shadesof.blue`,
+    };
+  },
+  computed: {
+    disam() {
+      return Array.isArray(this.color);
+    },
+    text() {
+      const c = Color(this.color.value);
+      return `text-${c.isLight() ? "black" : "white"}`;
+    },
+  },
+  methods: {
+    toHex(blue) {
+      const b = Color(blue);
+      return b.hex();
+    },
   },
 };
 </script>
@@ -77,5 +119,11 @@ export default {
 pre {
   color: #fff;
   background-color: #333;
+}
+section {
+  height: calc(100vh - 5rem);
+}
+.swatch {
+  aspect-ratio: 1/1;
 }
 </style>
