80d78d738da7082f9a18be32f474c68bc36b8e1a
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-05-20 21:28:38 -0400

    Update Fill.vue
    add color stats

--- components/Fill.vue
+++ components/Fill.vue
@@ -1,14 +1,22 @@
 <template>
   <div
-    class="flex flex-col text-center w-full h-full p-12"
+    class="flex flex-col w-full h-full p-12"
     :class="text"
     :style="`background-color:${blue.value}`"
   >
-    <slot></slot>
-    <p class="text-2xl">{{ hex }}</p>
-    <p class="mt-auto">
-      <template v-if="blue.alias"
-      >AKA {{ blue.alias }}<br/></template>
+    <div class="flex justify-between">
+      <div>
+        <slot></slot>
+        <p class="text-2xl">{{ hex }}</p>
+      </div>
+      <div class="text-right">
+        <p>Hue {{ hue }}</p>
+        <p>This blue is <template v-if="oob">beyond </template>{{ shade }}</p>
+        <p v-if="isGray">This blue is gray</p>
+      </div>
+    </div>
+    <p class="mt-auto text-right">
+      <template v-if="blue.alias">AKA {{ blue.alias }}<br /></template>
       <span class="text-sm">from {{ blue.source }}</span>
     </p>
   </div>
@@ -19,19 +27,40 @@ import Color from "color";
 export default {
   props: {
     blue: {
-      type: String,
+      type: Object,
       required: true,
     },
   },
   computed: {
+    b() {
+      return Color(this.blue.value);
+    },
     text() {
       const c = Color(this.blue.value);
       return `text-${c.isLight() ? "black" : "white"}`;
     },
     hex() {
-      const b = Color(this.blue.value);
-      return b.hex();
+      return this.b.hex();
+    },
+    hue() {
+      return Math.round(this.b.hsl().object().h);
+    },
+    shade() {
+      return this.hue <= 195
+        ? "cyan"
+        : this.hue > 195 && this.hue <= 225
+        ? "azure"
+        : "blue";
+    },
+    isGray() {
+      const hsl = this.b.hsl().object();
+      return (
+        hsl.s <= 22 || hsl.l <= 10 || (hsl.l > 10 && hsl.l <= 15 && hsl.s <= 50)
+      );
+    },
+    oob() {
+      return this.hue < 170 || this.hue > 250;
     }
-  }
+  },
 };
 </script>
