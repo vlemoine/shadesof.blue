a4237a935d916d4aa63195244ca4674da6345878
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-05-22 21:38:06 -0400

    Update index.vue
    focus style updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="px-8">
     <div
-      class="swatches__header sticky top-0 z-10 flex flex-wrap -mx-8 py-4 px-8"
+      class="swatches__header sticky top-0 z-10 flex flex-wrap -mx-8 py-4 px-8 gap-4"
     >
-      <label for="name"
-        >Filter
+      <label for="name" class="flex items-center gap-3"
+        ><i class="fa-duotone fa-filters"></i> Filter
         <input
           id="name"
           v-model="filters.name"
           name="name"
           type="search"
-          class="text-black"
+          class="text-black px-4 py-1 rounded-full focus-within:shadow-focus focus:outline-none"
       /></label>
       <button
-        class="appearance-none mx-4 border"
+        :class="[classes.pill, filters.open ? classes.pillSelected : '']"
         @click="filters.open = !filters.open"
       >
         Options
-        <i class="fas fa-filter"></i>
       </button>
       <p class="col-span-3 ml-auto total">
         Displaying <strong>{{ count }}</strong> / {{ c.length }} blues
@@ -30,6 +29,7 @@
           <template v-for="(f, i) in filters.families.options">
             <label
               :key="i"
+              class="color-checkbox "
               :class="[
                 classes.pill,
                 filters.families.selected.includes(f)
@@ -43,7 +43,7 @@
                 v-model="filters.families.selected"
                 type="checkbox"
                 :value="f"
-                class="appearance-none rounded-full w-6 h-6 mr-2 filter"
+                class="appearance-none rounded-full w-6 h-6 mr-2 filter focus:outline-none"
                 :class="`filter--${f}`"
               />
               {{ f }}
@@ -224,7 +224,7 @@ export default {
         "HTML/Web/X11",
       ],
       classes: {
-        pill: "px-2 py-1 flex items-center cursor-pointer rounded-full border border-gray-300 dark:border-gray-700",
+        pill: "px-2 py-1 flex items-center cursor-pointer rounded-full border border-gray-400 dark:border-gray-600 focus-within:shadow-focus focus:outline-none",
         pillSelected: "bg-gray-300 dark:bg-gray-700 font-bold",
       },
     };
@@ -276,10 +276,14 @@ export default {
 
 <style>
 :root {
+  --focus-ring: hsl(210 100% 50% / 0.33);
   --swatch-width: 5rem;
   --swatch-aspect: 1 / 1;
   --swatch-aspect-pc: calc(var(--swatch-aspect) * 100%);
   --swatch-zoom: calc(var(--swatch-width) * 1.8);
+}
+.dark {
+  --focus-ring: hsl(210 100% 50% / 0.67);
 }
 .swatches {
   grid-template-columns: repeat(auto-fill, minmax(var(--swatch-width), 1fr));
