2b5682c7fd680265341a7cd70e2715bb968b80d5
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-05-22 21:37:30 -0400

    Update Header.vue
    consolidate styles

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,28 @@
 <template>
-  <header class="flex py-4 px-8 h-20 border-b border-black dark:border-white">
-      <NuxtLink to="/" class="flex items-center"
-        ><Logo />
-        <h1 class="ml-4 text-2xl font-bold">Shades of Blue</h1></NuxtLink
-      >
-      <nav class="flex ml-auto items-center gap-4">
-        <NuxtLink to="/random" class="font-bold text-lg">Random</NuxtLink>
-        <NuxtLink to="/about" class="font-bold text-lg">About</NuxtLink>
-        <ThemeToggle />
-      </nav>
-    </header>
+  <header
+    class="flex py-4 px-8 h-20 border-b border-black dark:border-white items-center"
+  >
+    <NuxtLink
+      to="/"
+      class="flex items-center focus-within:shadow-focus focus:outline-none"
+      ><Logo />
+      <h1 class="ml-4 text-2xl font-bold">Shades of Blue</h1></NuxtLink
+    >
+    <nav class="flex ml-auto items-center gap-4">
+      <NuxtLink to="/random" :class="link">Random</NuxtLink>
+      <NuxtLink to="/about" :class="link">About</NuxtLink>
+      <ThemeToggle />
+    </nav>
+  </header>
 </template>
 
-<style>
-header {
-  align-items: center;
-}
-</style>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      link: "font-bold text-lg focus-within:shadow-focus focus:outline-none",
+    };
+  },
+};
+</script>
