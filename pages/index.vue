7e53aab96c884d48f70f02f81009910e6ccb1204
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-06-04 23:59:57 -0400

    update gray logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,110 +1,97 @@
 <template>
   <div class="px-4 lg:px-8">
     <div
-      class="
-        swatches__header | py-4
-        [ sticky top-0 z-10 ]
-        [ flex flex-wrap gap-4 justify-center ]
-        [ -mx-4 px-4 ]
-        [ lg:-mx-8 lg:px-8 ]
-      "
+      class="swatches__header | py-4 [ sticky top-0 z-10 ] [ flex flex-wrap gap-4 justify-center ] [ -mx-4 px-4 ] [ lg:-mx-8 lg:px-8 ]"
     >
       <div class="flex gap-3 [ w-full | lg:w-auto ]">
-      <label for="name" class="flex items-center gap-3 [ w-full | lg:w-auto ]"
-        ><i class="fa-duotone fa-filters"></i> Filter
-        <input
-          id="name"
-          v-model="filters.name"
-          name="name"
-          type="search"
-          class="
-            rounded-full dark:bg-gray-800  
-            [ text-black dark:text-white ]
-            [ w-full lg:w-auto ]
-            [ px-4 py-1 ]
-            [ focus-within:shadow-focus focus:outline-none ]
-            [ border border-gray-900 dark:border-gray-600 ]
-          "
-      /></label>
-      <button
-        class="pl-3 pr-3"
-        :class="[
-          classes.pill,
-          filters.open ? classes.pillSelected : 'bg-white dark:bg-black',
-        ]"
-        @click="filters.open = !filters.open"
-      >
-        Options
-      </button>
+        <label for="name" class="flex items-center gap-3 [ w-full | lg:w-auto ]"
+          ><i class="fa-duotone fa-filters"></i> Filter
+          <input
+            id="name"
+            v-model="filters.name"
+            name="name"
+            type="search"
+            class="rounded-full dark:bg-gray-800 [ text-black dark:text-white ] [ w-full lg:w-auto ] [ px-4 py-1 ] [ focus-within:shadow-focus focus:outline-none ] [ border border-gray-900 dark:border-gray-600 ]"
+        /></label>
+        <button
+          class="pl-3 pr-3"
+          :class="[
+            classes.pill,
+            filters.open ? classes.pillSelected : 'bg-white dark:bg-black',
+          ]"
+          @click="filters.open = !filters.open"
+        >
+          Options
+        </button>
       </div>
       <p class="total | lg:ml-auto text-center">
         Displaying <strong>{{ count }}</strong> / {{ c.length }} blues
         documented!
       </p>
       <Filters :open="filters.open">
-        <div class=" py-1 flex items-center gap-3">
+        <div class="py-1 flex items-center gap-3">
           <strong class="whitespace-nowrap">Color family</strong>
           <div class="flex gap-2 items-center">
-          <div class="flex flex-wrap gap-3">
-          <template v-for="(f, i) in filters.families.options">
-            <label
-              :key="i"
-              class="color-checkbox"
-              :class="[
-                classes.pill,
-                filters.families.selected.includes(f)
-                  ? classes.pillSelected
-                  : '',
-              ]"
-              :for="`family_${f}`"
-            >
-              <input
-                :id="`family_${f}`"
-                v-model="filters.families.selected"
-                type="checkbox"
-                :value="f"
-                class="appearance-none rounded-full w-6 h-6 mr-2 filter focus:outline-none"
-                :class="`filter--${f}`"
-              />
-              {{ f }}
-            </label>
-          </template>
-          </div>
-          <div
-            class="hidden md:block border-r h-6 border-opacity-80 dark:border-opacity-20"
-          ></div>
-          <div class="flex flex-wrap gap-3">
-            <button
-            :class="[
-              classes.pill,
-              filters.gray !== 0 ? classes.pillSelected : '',
-            ]"
-            @click="toggleGray()"
-          >
-            <span
-              class="rounded-full w-6 h-6 mr-2 bg-gray-300 dark:bg-gray-500 inline-flex items-center justify-center"
-              ><i class="fas fa-adjust"></i
-            ></span>
-            {{ filters.gray === 2 ? "Only grays" : "Grays" }}
-          </button>
-          <label
-            for="oob"
-            class="px-4 flex items-center"
-            :class="[classes.pill, filters.oob ? classes.pillSelected : '']"
-          >
-            <input
-              id="oob"
-              v-model="filters.oob"
-              type="checkbox"
-              class="appearance-none"
-            />
-            <span
-              class="rounded-full w-6 h-6 mr-2 inline-flex items-center justify-center filter--oob"
-              ><i class="fas fa-rainbow"></i
-            ></span>
-            Out of bounds
-          </label>
-          </div>
+            <div class="flex flex-wrap gap-3">
+              <template v-for="(f, i) in filters.families.options">
+                <label
+                  :key="i"
+                  class="color-checkbox"
+                  :class="[
+                    classes.pill,
+                    filters.families.selected.includes(f)
+                      ? classes.pillSelected
+                      : '',
+                  ]"
+                  :for="`family_${f}`"
+                >
+                  <input
+                    :id="`family_${f}`"
+                    v-model="filters.families.selected"
+                    type="checkbox"
+                    :value="f"
+                    class="appearance-none rounded-full w-6 h-6 mr-2 filter focus:outline-none"
+                    :class="`filter--${f}`"
+                  />
+                  {{ f }}
+                </label>
+              </template>
+            </div>
+            <div
+              class="hidden md:block border-r h-6 border-opacity-80 dark:border-opacity-20"
+            ></div>
+            <div class="flex flex-wrap gap-3">
+              <button
+                :class="[
+                  classes.pill,
+                  filters.gray !== 0 ? classes.pillSelected : '',
+                ]"
+                @click="toggleGray()"
+              >
+                <span
+                  class="rounded-full w-6 h-6 mr-2 bg-gray-300 dark:bg-gray-500 inline-flex items-center justify-center"
+                  ><i class="fas fa-adjust"></i
+                ></span>
+                {{ filters.gray === 2 ? "Only grays" : "Grays" }}
+              </button>
+              <label
+                for="oob"
+                class="px-4 flex items-center"
+                :class="[classes.pill, filters.oob ? classes.pillSelected : '']"
+              >
+                <input
+                  id="oob"
+                  v-model="filters.oob"
+                  type="checkbox"
+                  class="appearance-none"
+                />
+                <span
+                  class="rounded-full w-6 h-6 mr-2 inline-flex items-center justify-center filter--oob"
+                  ><i class="fas fa-rainbow"></i
+                ></span>
+                Out of bounds
+              </label>
+            </div>
           </div>
         </div>
         <div class="flex items-center py-2 flex-wrap">
@@ -140,9 +127,12 @@
           }"
           :show-labels="filters.check.showLabels.value"
         >
-          <!-- {{blue.family}} -->
+        <!-- {{blue.hsv}} -->
+          <!-- s {{ Math.round(blue.hsv.s) }}<br /> -->
+          <!-- w {{ Math.round(blue.hwb.w) }}<br />
+          b {{ Math.round(blue.hwb.b) }}<br /> -->
+          <!-- w+b {{ Math.round(blue.hwb.b + blue.hwb.w) }} -->
           <!-- {{blue.dupe}} -->
-          <!-- {{blue.hsl.h}} -->
           <!-- <span v-if="blue.del">del</span> -->
         </Swatch>
       </template>
@@ -152,6 +142,7 @@
 
 <script>
 import Color from "color";
+import isGray from "../scripts/gray.js";
 
 const Panel = {
   showLabels: { label: "Show labels", value: false },
@@ -159,17 +150,34 @@ const Panel = {
 
 const filterLogic = (blue, filters) => {
   // If returns true, it will hide the swatch from the view!
+  const query = filters.name.toLowerCase();
+  const gray = query.search("gray") !== -1;
+  const grey = query.search("grey") !== -1;
+  let _gray = query;
+  if (gray) {
+    _gray = query.replace("gray", "grey");
+  } else if (grey) {
+    _gray = query.replace("grey", "gray");
+  }
+  const _blue =
+    blue.family === "Blue" && !filters.families.selected.includes("Blue");
+  const _azure =
+    blue.family === "Azure" && !filters.families.selected.includes("Azure");
+  const _cyan =
+    blue.family === "Cyan" && !filters.families.selected.includes("Cyan");
+  const _hex = blue.hex.toLowerCase();
+  const _alias = blue.alias.toLowerCase();
+  const _title = blue.title.toLowerCase();
+  const search =
+    _hex.search(query) === -1 &&
+    _alias.search(query) === -1 &&
+    _title.search(query) === -1 &&
+    _hex.search(_gray) === -1 &&
+    _alias.search(_gray) === -1 &&
+    _title.search(_gray) === -1;
   return (
-    (filters.families.selected.length !== 0 &&
-      ((blue.family === "Blue" &&
-        !filters.families.selected.includes("Blue")) ||
-        (blue.family === "Azure" &&
-          !filters.families.selected.includes("Azure")) ||
-        (blue.family === "Cyan" &&
-          !filters.families.selected.includes("Cyan")))) ||
-    (blue.hex.toLowerCase().search(filters.name.toLowerCase()) === -1 &&
-      blue.alias.toLowerCase().search(filters.name.toLowerCase()) === -1 &&
-      blue.title.toLowerCase().search(filters.name.toLowerCase()) === -1) ||
+    (filters.families.selected.length !== 0 && (_blue || _azure || _cyan)) ||
+    search ||
     (blue.gray && !filters.gray) ||
     (!blue.gray && filters.gray === 2) ||
     (blue.oob && !filters.oob)
@@ -260,23 +268,19 @@ export default {
         const b = Color(s.value);
         s.alias = s.alias || "";
         s.hex = b.hex();
-        s.hsl = {};
-        ["h", "s", "l"].forEach((v) => {
-          s.hsl[v] = Math.round(b.hsl().object()[v]);
-        });
-        s.gray =
-          s.hsl.s <= 22 ||
-          s.hsl.l <= 10 ||
-          (s.hsl.l > 10 && s.hsl.l <= 15 && s.hsl.s <= 50);
-        s.oob = s.hsl.h < 170 || s.hsl.h > 250;
-        s.del = s.hsl.h < 160 || s.hsl.h > 260;
+        s.hsv = b.hsv().object();
+        s.hwb = b.hwb().object(); // debug
+        s.gray = isGray(s.value);
+        const hue = s.hsv.h;
+        s.oob = hue < 170 || hue > 250;
+        s.del = hue < 160 || hue > 260;
         s.nameDupe = sort.filter((e) => e.slug === s.slug).length > 1;
-        const h = s.hsl.h;
-        s.family = h <= 195 ? "Cyan" : h > 195 && h <= 225 ? "Azure" : "Blue";
+        s.family =
+          hue <= 195 ? "Cyan" : hue > 195 && hue <= 225 ? "Azure" : "Blue";
       });
-      sort = sort.sort((a, b) => b.hsl.l - a.hsl.l);
-      sort = sort.sort((a, b) => a.hsl.s - b.hsl.s);
-      sort = sort.sort((a, b) => a.hsl.h - b.hsl.h);
+      sort = sort.sort((a, b) => Math.round(b.hsv.v) - Math.round(a.hsv.v));
+      sort = sort.sort((a, b) => Math.round(a.hsv.s) - Math.round(b.hsv.s));
+      sort = sort.sort((a, b) => Math.round(a.hsv.h) - Math.round(b.hsv.h));
       return sort;
     },
     count() {
@@ -305,11 +309,6 @@ export default {
   --swatch-aspect: 1 / 1;
   --swatch-multiply: 1.5;
   --swatch-zoom: calc(var(--swatch-width) * var(--swatch-multiply));
-}
-@media (min-width: 768px) {
-  :root {
-    /* --swatch-multiply: 1.8; */
-  }
 }
 @media (min-width: 1024px) {
   :root {
