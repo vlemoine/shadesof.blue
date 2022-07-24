856d2aa3968be9b8de2b598669bc5e1bf580f8cf
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-07-23 20:51:27 -0400

    stylelint v14 updates

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
+    "stylelint-config-standard",
+    "stylelint-config-prettier",
+    "stylelint-config-html/vue",
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
-}
+  rules: {
+    "alpha-value-notation": "number",
+    "selector-class-pattern": null
+  },
+};
