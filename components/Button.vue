f82367402753afb7fb6c45f28ece0d74faa4a9e0
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-15 10:49:37 -0500

    Create Button.vue

---
+++ components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button :class="[cls, active ? activeCls : '']" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+const classes = `
+  pl-3 pr-3 px-2 py-1
+  flex items-center
+  cursor-pointer
+  rounded-full
+  border border-gray-900 dark:border-gray-600
+  focus-within:shadow-focus focus:outline-none
+  bg-white dark:bg-black
+`;
+const activeClasses = "bg-gray-300 dark:bg-gray-700 font-bold";
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      cls: classes,
+      activeCls: activeClasses,
+    };
+  },
+};
+</script>
